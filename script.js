6bf42d64b2a69bc650772b84ab2c26c608f51c94
DonDuck1 <malj0042@hz.nl>
2022-09-14 23:33:09 +0100

    Fixed the fix of the aside menu: should work (again) now

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 setTimeout( function() { imgremoval1(); }, 250);
 setTimeout( function() { imgremoval2(); }, 250);
 function imgremoval1() {
-    var element = document.getElementById("animationfixjs");
-    element.classList.remove("animationfix");
+    var imgremove1 = document.getElementById("animationfixjs");
+    imgremove1.classList.remove("animationfix");
   } 
-function imgremoval2() {
-    var element = document.getElementById("animationfixjs");
-    element.classList.add("animationfixnodisplay");
-} 
+  function imgremoval2() {
+    var imgremove2 = document.getElementById("animationfixjs");
+    imgremove2.classList.add("animationfixnodisplay");
+}
+setTimeout( function() { imgremoval3(); }, 250);
+setTimeout( function() { imgremoval4(); }, 250);
+function imgremoval3() {
+    var imgremove3 = document.getElementById("animationfixdivjs");
+    imgremove3.classList.remove("animationfixdiv");
+  } 
+  function imgremoval4() {
+    var imgremove4 = document.getElementById("animationfixdivjs");
+    imgremove4.classList.add("animationfixdivnodisplay");
+}
