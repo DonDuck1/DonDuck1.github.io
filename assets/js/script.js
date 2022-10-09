714f38b99229f212404e97de4ee57678d6362d0b
DonDuck1 <malj0042@hz.nl>
2022-10-09 20:40:17 +0100

    website is now (hopefully) done and functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 setTimeout( function() { imgremoval1(); }, 250);
 setTimeout( function() { imgremoval2(); }, 250);
 function imgremoval1() {
-    var imgremove1 = document.getElementById("animationfixjs");
-    imgremove1.classList.remove("animationfix");
-} 
-function imgremoval2() {
-    var imgremove2 = document.getElementById("animationfixjs");
-    imgremove2.classList.add("animationfixnodisplay");
+	var imgremove1 = document.getElementById("animationfixjs");
+	imgremove1.classList.remove("animationfix");
 }
-setTimeout( function() { imgremoval3(); }, 250);
-setTimeout( function() { imgremoval4(); }, 250);
+function imgremoval2() {
+	var imgremove2 = document.getElementById("animationfixjs");
+	imgremove2.classList.add("animationfixnodisplay");
+}
+setTimeout(function () { imgremoval3(); }, 250);
+setTimeout(function () { imgremoval4(); }, 250);
 function imgremoval3() {
-    var imgremove3 = document.getElementById("animationfixdivjs");
-    imgremove3.classList.remove("animationfixdiv");
-  } 
-  function imgremoval4() {
-    var imgremove4 = document.getElementById("animationfixdivjs");
-    imgremove4.classList.add("animationfixdivnodisplay");
+	var imgremove3 = document.getElementById("animationfixdivjs");
+	imgremove3.classList.remove("animationfixdiv");
+}
+function imgremoval4() {
+	var imgremove4 = document.getElementById("animationfixdivjs");
+	imgremove4.classList.add("animationfixdivnodisplay");
 }
